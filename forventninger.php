9d206fa58c49cfd79b7cb2727775532b75d51481
Audun Wangen <audun.wangen@kongsvinger.kommune.no>
2010-06-01 16:19:46 +0200

    Added ToDo-list in Readme-file

--- forventninger.php
+++ forventninger.php
@@ -2,26 +2,26 @@
 include 'inc/header.php';
 ?>
 <table width="675" border="0" cellpadding="3" cellspacing="0" bgcolor="#FFFFFF">
-<TR>
-	<TD width="675"><br><br>
-</TR>
-</TABLE>
+<tr>
+	<td width="675"><br /><br />
+</td>
+</tr>
+</table>
 
 <table width="675" border="0" cellpadding="3" cellspacing="0" bgcolor="#FFFFFF">
 <tr><td valign="top">
-<span class="heading-stor">Vedlikehold av kommunale veier.</span><br><br>
-KOMMUNALTEKNIKK KAN OGSÅ HJELPE DEG PÅ OMRÅDER SOM GJELDER<br>
-vann, kloakk, renovasjon, parker, lekeplasser, friareal<br><br>
-BEREDSKAP<br>
-Kommunalteknikk har døgnkontinuerlig beredskapsvakt for å ta seg av hendelser på kommunale anlegg.<br>
-Beredskapstelefonen er 35 59 34 00.<br><br>
-KJØR FORSIKTIG OG VÆR SPESIELT AKTSOM I OMRÅDER DER BARN FERDES!<br><br>
-Med hilsen<br>
-Torbjørn Krogstad<br>
-Leder Kommunalteknikk
-<br><br>
-<A HREF="http://www.porsgrunn.kommune.no/Tjenester/Tjenesteerklaringer-Porsgrunn/Kommunale-veier-tjenesteerklaring/" target="_new" alt="Les Tjenesteerklæring her">Se Tjenesteerklæring for Kommunalteknikk i Porsgrunn kommune.</A><BR><BR>
-Denne tjenesten leveres av virksomheten <A HREF="http://www.porsgrunn.kommune.no/Kommunens-organisasjon/Radmann/By--og-kulturutvikling/Kommunalteknikk/" target="_top">Kommunalteknikk</A>.
+<span class="heading-stor">Vedlikehold av kommunale veier.</span><br /><br />
+KOMMUNALTEKNIKK KAN OGSÅ HJELPE DEG PÅ OMRÅDER SOM GJELDER<br />
+vann, kloakk, renovasjon, parker, lekeplasser, friareal<br /><br />
+BEREDSKAP<br />
+Kommunalteknikk har døgnkontinuerlig beredskapsvakt for å ta seg av hendelser på kommunale anlegg.<br />
+Beredskapstelefonen er FIXME.<br /><br />
+KJØR FORSIKTIG OG VÆR SPESIELT AKTSOM I OMRÅDER DER BARN FERDES!<br /><br />
+Med hilsen<br />
+FIXME FIXMEsen<br />
+<br /><br />
+<a href="http://FIXME" target="_new">Se Tjenesteerklæring for Kommunalteknikk i FIXME kommune.</a><br /><br />
+Denne tjenesten leveres av virksomheten <a href="http://FIXME" target="_top">FIXME-virksomheten</a>.
 </td></tr>
 </table>
 <?
