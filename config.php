3edd75911f6774fc5322a3af14083caac931915c
Audun Wangen <audun.wangen@kongsvinger.kommune.no>
2010-06-02 15:25:54 +0200

    Added tracking for config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$dbusername = "fiksgate";
+$dbpassword = "fiksgate";
+$dbname = "fiksgate";
+$tbname = "fiksgrafitti";
+
+connecttodb($servername,$dbname,$dbusername,$dbpassword);
+function connecttodb($servername,$dbname,$dbuser,$dbpassword)
+{
+global $link;
+$link=mysql_connect ("$servername","$dbuser","$dbpassword");
+if(!$link){die("Could not connect to MySQL");}
+mysql_select_db("$dbname",$link) or die ("could not open db".mysql_error());
+}
+
+
+?>
