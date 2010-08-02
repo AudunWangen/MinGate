cb502415e9482f30f9d9c77b5d1516b3ffa3ec91
Audun Wangen <audun.wangen@gmail.com>
2010-08-02 11:47:50 +0200

    Fixed admin/login bug

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,9 @@
 <?php
-<?
+ini_set('display_errors',1);
 session_start();
-if(!session_is_registered(myusername)){
+if(!session_is_registered('myusername')){
 header("location:main_login.php");
 }
-?>
 
 require '../config.php';
 
