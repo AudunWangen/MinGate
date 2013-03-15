4715ce23e276f86276010e40b63c9bfadf1a28fe
Audun Wangen <audun.wangen@gmail.com>
2013-03-15 15:47:41 +0100

    Forgot to add tracking for some files, and also added the empty folders update/thumbs for easier setup.

---
+++ explanation.php
@@ -0,0 +1,5 @@
+                               <h3>Mitt nærområde</h3>
+                                <p>Her kan du melde inn mangler eller feil som feks. hull i veg, søppel og tagging m.m. <br />Sakene behandles fortløpende av kommunen.</p>
+                                <h3>Hvordan melde inn en ny sak?</h3>
+                                <p>Du kan melde inn et problem ved å benytte vårt <a href="submit.php">elektroniske skjema</a>.</p>
+
